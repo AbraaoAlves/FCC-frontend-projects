cbb11b83ed3af297d1f1b1d3bd8fa5f9eb572dfc
AbraaoAlves <abraao.teodosio@gmail.com>
2024-03-29 21:09:18 -0300

    config: vscode vitest extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 ///<reference types="vitest"/>
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.js"
+    setupFiles: ["src/setup.ts"],
+    include: ["src/**/*.test.{ts,js,tsx,jsx}"],
+    typecheck: {
+      enabled: false
+    },
   }  
-})
+});
