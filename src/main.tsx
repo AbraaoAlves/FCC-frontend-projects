f40965432f2940fd96740a933d519a55f46d068c
AbraaoAlves <abraao.teodosio@gmail.com>
2024-03-29 22:40:08 -0300

    chore: use env var to set base url

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>    
   </React.StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const BASE = process.env.BASE_URL?? '/'; 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: BASE,
   test: {
     globals: true,
     environment: "jsdom",
