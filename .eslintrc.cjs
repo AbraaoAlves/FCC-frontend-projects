3345fd48c10617fed2745aba1e4a372e1f8f7faa
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-02 21:20:13 -0300

    config: connect eslint and vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,29 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:vitest-globals/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/*.{test,spec}.{j,t}s?(x)"],
+      env: {
+        browser: true,
+        es2020: true,
+        "vitest-globals/env": true,
+      },
+    },
+  ],
+};
