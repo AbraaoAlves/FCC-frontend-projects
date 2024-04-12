14724e4202bf0f35f7bb03cecba214f46c836063
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-11 22:20:15 -0300

    refactor: rewrite function to get more control of reverse process

--- src/pages/Palindrome/utils.js
+++ src/pages/Palindrome/utils.js
@@ -24,5 +24,17 @@ export function checkIsPalindrome(value = "") {
  * @returns {string}
  */
 export function getPalindrome(value = "") {
-  return value.split("").reverse().join("");
+  let chars = value.split("");
+  let left = 0;
+  let right = chars.length - 1;
+
+  while (left < right) {
+    let temp = chars[left];
+    chars[left] = chars[right];
+    chars[right] = temp;
+    left++;
+    right--;
+  }
+
+  return chars.join("");
 }
--- src/pages/Palindrome/utils.test.js
+++ src/pages/Palindrome/utils.test.js
@@ -1,7 +1,10 @@
 import { getPalindrome } from "./utils";
 
 describe("getPalindrome", () => {
-  it.todo("should not rever ( and )", () => {
+  it("should reverse a common string like 123456", () => {
+    expect(getPalindrome("123456")).toBe("654321");
+  });
+  it.todo("should not reverse ( and )", () => {
     expect(getPalindrome("12(34)56")).toBe("65(43)21");
   });
 });
